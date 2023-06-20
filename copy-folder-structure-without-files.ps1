3a082d5bda50a1b0c4e1e53e5ee66e6e2d07395e
Michael D <McoreD@users.noreply.github.com>
2023-06-20 15:16:06 +0800

    $folderLevel

--- copy-folder-structure-without-files.ps1
+++ copy-folder-structure-without-files.ps1
@@ -3,7 +3,9 @@ param (
     [string]$sourceDir,
 
     [Parameter(Mandatory=$true)]
-    [string]$destDir
+    [string]$destDir,
+
+    [int]$folderLevel = -1
 )
 
 # Remove quotation marks from the directory paths
@@ -18,19 +20,21 @@ if (!(Test-Path -Path $sourceDir -PathType Container)) {
 
 # Ensure that the destination directory exists, create it if it doesn't
 if (!(Test-Path -Path $destDir -PathType Container)) {
-    New-Item -ItemType Directory -Path $destDir | Out-Null
+    New-Item -ItemType Directory -Path $destDir -ErrorAction SilentlyContinue | Out-Null
 }
 
-# Get all subdirectories under the source directory up to two levels deep
-$sourceDirs = Get-ChildItem -Path $sourceDir -Recurse -Directory -Depth 2 |
-    Where-Object { ($_.FullName -ne $sourceDir) -and ($_.FullName.Split('\').Count -le $sourceDir.Split('\').Count + 2) }
+# Get all subdirectories under the source directory up to the specified levels deep
+if ($folderLevel -ge 0) {
+    $sourceDirs = Get-ChildItem -Path $sourceDir -Recurse -Directory -Depth $folderLevel |
+        Where-Object { ($_.FullName -ne $sourceDir) -and ($_.FullName.Split('\').Count -le $sourceDir.Split('\').Count + $folderLevel) }
+} else {
+    $sourceDirs = Get-ChildItem -Path $sourceDir -Recurse -Directory
+}
 
 foreach ($dir in $sourceDirs) {
     # Replace source directory path with destination in each subdirectory path
     $destSubDir = $dir.FullName.Replace($sourceDir, $destDir)
 
     # Create the subdirectory under destination if it does not exist
-    if (!(Test-Path -Path $destSubDir -PathType Container)) {
-        New-Item -ItemType Directory -Path $destSubDir | Out-Null
-    }
+    New-Item -ItemType Directory -Path $destSubDir -ErrorAction SilentlyContinue | Out-Null
 }
